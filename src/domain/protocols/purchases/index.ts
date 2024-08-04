86f86d68037f343a14c12c0c544ca1cac6a376e1
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-04 19:10:24 -0300

    chore: adding index.ts

---
+++ src/domain/protocols/purchases/index.ts
@@ -0,0 +1,5 @@
+export * from './getPurchases.interface'
+export * from './getPurchaseById.interface'
+export * from './getPurchasesByCustomer.interface'
+export * from './getPurchasesByDate.interface'
+export * from './getPurchasesByStatus.interface'
