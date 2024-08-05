95aab09e4a3eb0bc1528d664db251eb636b7c653
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-04 22:58:59 -0300

    feat: implementing search in elasticsearch

--- src/data/usecases/purchases/getPurchases.ts
+++ src/data/usecases/purchases/getPurchases.ts
@@ -1,7 +1,14 @@
+import { Purchase } from "@/domain/models/purchases";
 import { IGetPurchasesProtocol } from "@/domain/protocols/purchases";
+import elasticsearchClient from "@/infra/elasticsearch/elasticsearchClient";
 
 export class GetPurchases implements IGetPurchasesProtocol {
-    getPurchases(): Promise<IGetPurchasesProtocol.Result> {
-        throw new Error("Method not implemented.");
+    async getPurchases(): Promise<IGetPurchasesProtocol.Result> {
+        const response = await elasticsearchClient.search({
+            index: 'purchase_history'
+        });
+
+        const purchases = response.hits.hits.map((hit: any) => hit._source as Purchase);
+        return { purchases };
     }
 }
