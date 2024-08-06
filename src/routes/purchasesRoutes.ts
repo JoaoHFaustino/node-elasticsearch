dcd9330e1413dd1a92735f4dc9117a540d8c182a
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-05 21:40:00 -0300

    chore: adding controllers to the route

--- src/routes/purchasesRoutes.ts
+++ src/routes/purchasesRoutes.ts
@@ -2,9 +2,12 @@ import { PurchasesController } from "@/controllers/purchases/purchasesController
 import { Router } from "express";
 
 const purchaseRoutes = Router();
+const purchasesController = new PurchasesController();
 
-const productsController = new PurchasesController();
-
-purchaseRoutes.get('/purchases', (req, res) => productsController.getPurchases(req, res));
+purchaseRoutes.get('/purchases', (req, res) => purchasesController.getPurchases(req, res));
+purchaseRoutes.get('/purchases/customer/:customerId', (req, res) => purchasesController.getPurchasesByCustomer(req, res));
+purchaseRoutes.get('/purchases/date', (req, res) => purchasesController.getPurchasesByDate(req, res));
+purchaseRoutes.get('/purchases/status/:status', (req, res) => purchasesController.getPurchasesByStatus(req, res));
+purchaseRoutes.get('/purchases/:id', (req, res) => purchasesController.getPurchaseById(req, res));
 
 export default purchaseRoutes;
