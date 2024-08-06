2d046766e7c92e93a25d099081d715bd2d2e2109
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-05 21:37:42 -0300

    feat: implementing search by status

--- src/data/usecases/purchases/getPurchasesByStatus.ts
+++ src/data/usecases/purchases/getPurchasesByStatus.ts
@@ -1,8 +1,28 @@
+import { Purchase } from "@/domain/models/purchases";
 import { IGetPurchasesByStatusProtocol } from "@/domain/protocols/purchases";
+import elasticsearchClient from "@/infra/elasticsearch/elasticsearchClient";
 
 export class GetPurchasesByStatus implements IGetPurchasesByStatusProtocol {
-    getPurchasesByStatus(params: IGetPurchasesByStatusProtocol.Params): Promise<IGetPurchasesByStatusProtocol.Result> {
-        throw new Error("Method not implemented.");
-    }
+    async getPurchasesByStatus(params: IGetPurchasesByStatusProtocol.Params): Promise<IGetPurchasesByStatusProtocol.Result | null> {
+        try {
+            const response = await elasticsearchClient.search({
+                index: 'purchase_history',
+                body: {
+                    query: {
+                        match: { status: params.status }
+                    }
+                }
+            });
 
+            const purchases = response.hits.hits.map((hit: any) => hit._source as Purchase);
+
+            if (purchases.length === 0) {
+                return null;
+            }
+
+            return { purchases };
+        } catch (error: any) {
+            throw new Error(error.message || 'Error fetching purchase by STATUS from Elasticsearch');
+        }
+    }
 }
