f0984ce02e810f913228659ad354d6f02d508962
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-05 21:38:10 -0300

    feat: implementing search by date

--- src/data/usecases/purchases/getPurchasesByDate.ts
+++ src/data/usecases/purchases/getPurchasesByDate.ts
@@ -1,8 +1,33 @@
+import { Purchase } from "@/domain/models/purchases";
 import { IGetPurchasesByDateProtocol } from "@/domain/protocols/purchases";
+import elasticsearchClient from "@/infra/elasticsearch/elasticsearchClient";
 
 export class GetPurchasesByDate implements IGetPurchasesByDateProtocol {
-    getPurchasesByDate(params: IGetPurchasesByDateProtocol.Params): Promise<IGetPurchasesByDateProtocol.Result> {
-        throw new Error("Method not implemented.");
-    }
+    async getPurchasesByDate(params: IGetPurchasesByDateProtocol.Params): Promise<IGetPurchasesByDateProtocol.Result | null> {
+        try {
+            const response = await elasticsearchClient.search({
+                index: 'purchase_history',
+                body: {
+                    query: {
+                        range: {
+                            order_date: {
+                                gte: params.startDate,
+                                lte: params.endDate
+                            }
+                        }
+                    }
+                }
+            });
 
+            const purchases = response.hits.hits.map((hit: any) => hit._source as Purchase);
+
+            if (purchases.length === 0) {
+                return null;
+            }
+
+            return { purchases };
+        } catch (error: any) {
+            throw new Error(error.message || 'Error fetching purchase by ORDER_DATE from Elasticsearch');
+        }
+    }
 }
