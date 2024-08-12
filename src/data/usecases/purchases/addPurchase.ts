18b2d02d79a9fa280f928c6d9e83a063f60051bb
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-12 18:58:41 -0300

    fix: adjusting add purchase mapper

--- src/data/usecases/purchases/addPurchase.ts
+++ src/data/usecases/purchases/addPurchase.ts
@@ -5,15 +5,30 @@ export class AddPurchase implements IAddPurchaseProtocol {
     async addPurchase(params: IAddPurchaseProtocol.Params): Promise<IAddPurchaseProtocol.Result> {
         try {
             const { purchase } = params;
+
+            const items = purchase.items.map(item => ({
+                product_id: item.productId,
+                product_name: item.productName,
+                quantity: item.quantity,
+                price_per_unit: item.pricePerUnit,
+            }));
+
             await elasticsearchClient.index({
                 index: 'purchase_history',
                 id: purchase.orderId,
                 body: {
                     customer_id: params.purchase.customerId,
-                    items: params.purchase.items,
+                    items: items,
                     order_date: params.purchase.orderDate,
                     order_id: params.purchase.orderId,
-                    shipping_address: params.purchase.shippingAddress,
+                    shipping_address: {
+                        address_line_1: params.purchase.shippingAddress.addressLine1,
+                        address_line_2: params.purchase.shippingAddress.addressLine2,
+                        city: params.purchase.shippingAddress.city,
+                        state: params.purchase.shippingAddress.state,
+                        zip_code: params.purchase.shippingAddress.zipCode,
+                        country: params.purchase.shippingAddress.country
+                    },
                     status: params.purchase.status,
                     total_amount: params.purchase.totalAmount
                 }
